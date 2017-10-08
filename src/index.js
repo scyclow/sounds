5d93f52be5d7cbaf26366a97006d0826ad502cbf
scyclow <spike716@gmail.com>
2017-10-08 11:46:01 -0400

    basic audio

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 // @flow
 
-import st from './index.css'
+import './index.css'
 
-const something = async function (x: string) {
-  return console.log('bleh')
-}
+const AudioContext = window.AudioContext || window.webkitAudioContext;
+const ctx = new AudioContext();
 
-something('3')
+const source = ctx.createOscillator();
+const gain = ctx.createGain();
+
+source.connect(gain)
+gain.connect(ctx.destination)
+source.start()
